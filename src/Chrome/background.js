93e48fe42f305d72c36df4e20188d985311c6441
John30013 <jmpisello@gmail.com>
2019-05-25 01:50:42 -0400

    Check for stored config on install

--- src/Chrome/background.js
+++ src/Chrome/background.js
@@ -1,8 +1,15 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set(defaultConfig, () => {
-    console.debug("Default settings stored: %o", defaultConfig);
+  chrome.storage.sync.get(null, (config) => {
+    if (!Object.keys(config).length) {
+      chrome.storage.sync.set(defaultConfig, () => {
+        console.debug("Default config stored: %o", defaultConfig);
+      });
+    } else {
+      console.debug('Found existing config: %o', config);
+    }
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    // TODO: allow any page to match so config can be updated any time.
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
           css: ["#disqus_thread"]
