0d0ac96133cd96348d77019a5069ec8c2b256252
John30013 <jmpisello@gmail.com>
2019-05-26 22:05:04 -0400

    Bug fixes & major refactoring. Also added logic to clean up old (zombie) links.

--- src/Chrome/content.js
+++ src/Chrome/content.js
@@ -1,60 +1,94 @@
 let _options = {...defaultConfig},
   _timer = null,
   _observer = null,
-  _observedDuids = {};
+  _observedLinks = {};
 
-// Listen for option updates.
-chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
-  // _options.doDebug && console.debug('Got message: %o', msg);
-  if (msg.action === 'refreshOptions') {
-    refreshOptions();
-  }
-  else if (msg.action === 'logDebug' && msg.message) {
-    _options.doDebug && console.debug(`[${msg.caller}] ${msg.message}`, ...msg.params);
-  }
-});
-
-// Set up the IntersectionObserver to watch for auto-expand items coming into the viewport.
-_observer = new IntersectionObserver(function(entries, me) {
-  if (_options.checkInterval) {
-    entries.forEach(entry => {
-      const link = entry.target;
-      _options.doDebug && console.debug('Checking intersection of "%s" link %s', link.className, link.dataset.duid);
-      if (entry.isIntersecting) {
-        setTimeout(() => {
-          link.click();
-          link.title += ' clicked!';
-          me.unobserve(link);
-          delete _observedDuids[link.dataset.duid];
-          _options.doDebug && console.debug('--> Clicked %s (now %d observed)', link.dataset.duid, Object.keys(_observedDuids).length);
-        }, 100);
-      }
-    });
-  }
-}, {threshold: 1.0});
-
+// Listen for option updates and debug messages from config page.
+listenForMessages();
+// Set up the IntersectionObserver to watch for auto-expand links coming into view.
+createObserver();
 // Start the extension.
 refreshOptions();
 _options.doDebug && console.debug(`content.js is running in iframe ${window.name}.`);
-processNewShowLinks();
+processNewLinks();
 
-// End of main code.
+/* ===== End of main code. ===== */
 
-/* ====== Helper functions ====== */
-function processNewShowLinks() {
-  // Since processNewShowLinks() can be called by refreshOptions() when checkInterval is 
+/* ===== Helper functions. ===== */
+function listenForMessages() {
+  chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
+    // _options.doDebug && console.debug('Got message: %o', msg);
+    if (msg.action === 'refreshOptions') {
+      refreshOptions();
+    }
+    else if (msg.action === 'logDebug' && msg.message) {
+      _options.doDebug && console.debug(`[${msg.caller}] ${msg.message}`, ...msg.params);
+    }
+  });
+}
+
+function createObserver() {
+  _obserer = new IntersectionObserver(function (entries) {
+    if (_options.checkInterval) {
+      let foundIntersects = false;
+      entries.forEach(entry => {
+        const link = entry.target;
+        _options.doDebug && console.debug('Checking intersection of "%s" link %s', link.className, link.dataset.duid);
+        if (entry.isIntersecting) {
+          foundIntersects = true;
+          setTimeout(() => {
+            link.click();
+            link.title += ' clicked!';
+            _options.doDebug && console.debug('--> Clicked %s (now %d observed)', link.dataset.duid, Object.keys(_observedLinks).length, _observedLinks);
+            unobserveLink(link)
+          }, 100);
+        }
+      });
+      // Clean up old observed links. Disqus seems to output some (mostly "see more") 
+      // links that become hidden before they are clicked (e.g., zombie links). 
+      // This block unobserves all observed links that haven't been clicked after 
+      // five minutes. If some non-zombie links get unobserved, they will be  
+      // observed again the next time the processNewLinks timer fires.
+      if (foundIntersects) {
+        _options.doDebug && console.debug('--> Checking for old links');
+        const now = Date.now(),
+          maxAge = 5 * 60 * 1000,
+          oldLinkData = Object.keys(_observedLinks)
+            .filter(duid => now - duid >= maxAge)
+            .map(oldDuid => unobserveLink(_observedLinks[oldDuid]))
+          ;
+        _options.doDebug && console.debug('--> unobserved %s old links', oldLinkData.length, oldLinkData);
+      }
+    }
+    
+    /* ===== Helper function ===== */
+    // Removes a link from observation and all our record-keeping. (Reverses observeLink().)
+    // Returns an object with data about the unobserved link (currently used for debug output only).
+    function unobserveLink(link) {
+      const duid = link.dataset.duid;
+      _options.doDebug && console.debug('Unobserving "%s" link %s', link.className, duid);
+      _observer.unobserve(link);
+      delete _observedLinks[duid];
+      delete link.dataset.duid;
+      // link.title = link.title.replace(/(~\d+)+/, '');
+      return { 'link': link, 'duid': duid };
+    }
+  }, { threshold: 1.0 });
+
+}
+function processNewLinks() {
+  // Since processNewLinks() can be called by refreshOptions() when checkInterval is 
   // set to zero, clear any pending timeout first. It's a no-op if this call is
   // the result of the timer timing out.
   if (_timer) {
     window.clearTimeout(_timer);
   }
   if (!_options.checkInterval) {
-    _options.doDebug && console.debug('processNewShowLinks: stopping the timeout loop.');
+    _options.doDebug && console.debug('processNewLinks: stopping the timeout loop.');
     return;
   }
 
   // Find links to observe.
-  // _options.doDebug && console.debug('processNewShowLinks: starting.');
   let repliesSelector = [];
   if (_options.moreReplies) {
     repliesSelector.push('div.show-children-wrapper:not(.hidden) > a.show-children:not(.busy):not([data-duid])');
@@ -62,30 +96,32 @@ function processNewShowLinks() {
   if (_options.newReplies) {
     repliesSelector.push('a.realtime-button.reveal:not([style*="display: none;"]):not([data-duid])');
   }
-
+  // Observe replies links that haven't already been observed.
   if (repliesSelector.length) {
-    // Observe replies links that haven't already been observed.
     document.querySelectorAll(repliesSelector.join(',')).forEach(observeLink);
   }
   // Observe "see more" links that haven't already been observed.
+  const longItemsSelector = 'div.post-message-container:not([style*="max-height: none;"]) + a.see-more:not(.hidden):not([data-duid]), a.curtain-truncate:not(.hidden):not([data-duid])';
   if (_options.longItems) {
-    document.querySelectorAll('a.see-more:not(.hidden):not([data-duid]), a.curtain-truncate:not(.hidden):not([data-duid])').forEach(observeLink);
+    document.querySelectorAll(longItemsSelector).forEach(observeLink);
   }
-  // Check again after the checkInterval.
-  _timer = window.setTimeout(processNewShowLinks, _options.checkInterval * 1000);
-  // _options.doDebug && console.debug('processNewShowLinks: exiting.');
-}
+  // Reprocess after the checkInterval.
+  _timer = window.setTimeout(processNewLinks, _options.checkInterval * 1000);
 
-function observeLink(link) {
-  const duid = link.dataset.duid || 
-    (link.dataset.duid = (String.fromCharCode(Math.floor(Math.random() * 26) + 97) + Date.now()), link.dataset.duid);
-  if (!_observedDuids[duid]) {
-    link.title = duid;
-    _observer.observe(link);
-    _observedDuids[duid] = true;
-    _options.doDebug && console.debug('--> Start observing "%s" link %s (%d observed)', link.className, duid, Object.keys(_observedDuids).length);
-  } else {
-    _options.doDebug && console.warn('--> Already observing link %s!', link.className, duid)
+  /* ===== Helper function ===== */
+  // Observe a link. Instructs the IntersectionOserver to start observing the 
+  // link and does some record keeping.
+  function observeLink(link) {
+    let duid = link.dataset.duid;
+    if (!duid) {
+      link.dataset.duid = duid = Date.now();
+      // link.title += `~${duid}`;
+      _observer.observe(link);
+      _observedLinks[duid] = link;
+      _options.doDebug && console.debug('--> Start observing "%s" link %s (%d observed)', link.className, duid, Object.keys(_observedLinks).length, _observedLinks);
+    } else {
+      _options.doDebug && console.info('--> Already observing link %s!', link.className, duid)
+    }
   }
 }
 
@@ -94,7 +130,7 @@ function refreshOptions() {
     const oldCheckInterval = +_options.checkInterval;
     _options = options;
     if (oldCheckInterval === 0 && _options.checkInterval !== oldCheckInterval) {
-      processNewShowLinks();
+      processNewLinks();
     }
     _options.doDebug && console.debug("Got sync'd options: %o", _options);
   });
