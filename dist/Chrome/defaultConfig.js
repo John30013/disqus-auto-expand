743806abc4d265369f06e302d5863ae50a4b0f6c
John Pisello <jmpisello@gmail.com>
2019-05-30 13:54:31 -0400

    Latest build (removed mobile-only config options)

--- dist/Chrome/defaultConfig.js
+++ dist/Chrome/defaultConfig.js
@@ -1,1 +1,1 @@
-const defaultConfig={moreReplies:!0,newReplies:!0,longItems:!0,mobileMedia:!1,hideOpenedMobileMediaLinks:!0,checkInterval:5,useDarkTheme:!1,doDebug:!1};
+const defaultConfig={moreReplies:!0,newReplies:!0,longItems:!0,checkInterval:5,useDarkTheme:!1,doDebug:!1};
