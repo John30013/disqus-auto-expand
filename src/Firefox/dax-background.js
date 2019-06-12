c1e23e9dcd739fbf070d7d76a1e1fa5eb717e34c
John30013 <jmpisello@gmail.com>
2019-06-12 00:45:04 -0400

    Code reformatting.

--- src/Firefox/dax-background.js
+++ src/Firefox/dax-background.js
@@ -1,26 +1,28 @@
-let _config = defaultConfig,
-  _callers = {};
+let _config = defaultConfig;
 
 // Make sure the extension's options are stored when the extension starts up.
 browser.runtime.onInstalled.addListener(async () => {
-  // Make sure current configuration options are available in storage.
   try {
     _config = await browser.storage.sync.get(defaultConfig);
     if (!Object.keys(_config).length) {
       await browser.storage.sync.set(defaultConfig);
-      console.debug("Stored default config: %o", defaultConfig);
+      // console.debug("Stored default config: %o", defaultConfig);
     } else {
-      _config.doDebug && console.debug('Found existing config: %o', _config);
+      _config.doDebug && console.debug("Found existing config: %o", _config);
       // Check for and remove obsolete config items.
+      let obsoleteKeys = [];
       for (let key in _config) {
         if (!defaultConfig.hasOwnProperty(key)) {
-          await browser.storage.sync.remove(key);
-          _config.doDebug && console.debug('--> removed obsolete config value %s', key);
+          obsoleteKeys.push(key);
         }
       }
+      if (obsoleteKeys.length) {
+        await browser.storage.sync.remove(obsoleteKeys);
+        _config.doDebug &&
+          console.debug("--> removed obsolete config value %s", key);
+      }
     }
-  }
-  catch(error) {
+  } catch (error) {
     console.error(`Storage operation failed: ${error}.`);
   }
 });
