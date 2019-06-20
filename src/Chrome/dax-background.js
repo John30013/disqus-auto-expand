ff2e6c37bd26aa51ce97841816c26f5ff0f6d127
John30013 <jmpisello@gmail.com>
2019-06-20 01:03:39 -0400

    Added setIcon() function and message listener. - Fixed bugs with chrome.runtime.lastError logging.

--- src/Chrome/dax-background.js
+++ src/Chrome/dax-background.js
@@ -1,12 +1,12 @@
-let _config = defaultConfig;
-
-// Make sure the extension's options are stored when the extension starts up.
 chrome.runtime.onInstalled.addListener(() => {
+  // Make sure the extension's options are stored when the extension starts up.
+  // Pass `null` so we get everything in storage. This allows us to clean up
+  // obsolete config values (see else block below).
   chrome.storage.sync.get(null, config => {
     if (chrome.runtime.lastError) {
-      console.error(
+      console.warn(
         `Couldn't get configuration options from storage: ${
-          chrome.runtime.lastError
+          chrome.runtime.lastError.message
         }`
       );
       return;
@@ -15,9 +15,10 @@ chrome.runtime.onInstalled.addListener(() => {
       chrome.storage.sync.set(defaultConfig, () => {
         // console.debug("Stored default config: %o", defaultConfig);
       });
+      config = defaultConfig();
     } else {
-      _config.doDebug && console.debug("Found existing config: %o", config);
       // Check for and remove obsolete config items.
+      config.doDebug && console.debug("Found existing config: %o", config);
       let obsoleteKeys = [];
       for (let key in config) {
         if (!defaultConfig.hasOwnProperty(key)) {
@@ -27,9 +28,9 @@ chrome.runtime.onInstalled.addListener(() => {
       if (obsoleteKeys.length) {
         chrome.storage.sync.remove(obsoleteKeys, () => {
           if (chrome.runtime.lastError) {
-            console.error(
+            console.warn(
               `Couldn't remove obsolete keys from storage: ${
-                chrome.runtime.lastError
+                chrome.runtime.lastError.message
               }.`
             );
           } else {
@@ -42,7 +43,8 @@ chrome.runtime.onInstalled.addListener(() => {
         });
       }
     }
-  });
+    setIcon(!!config.checkInterval);
+  }); // end of chrome.storage.sync.get() callback.
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([
@@ -56,4 +58,31 @@ chrome.runtime.onInstalled.addListener(() => {
       },
     ]);
   });
-});
+
+  // Listen for messages from content and config scripts.
+  chrome.runtime.onMessage.addListener(msg => {
+    if (msg.action === "setIcon" && typeof msg.data !== "undefined") {
+      setIcon(!!msg.data);
+    }
+  });
+
+  // Set the extension's icon.
+  function setIcon(isRunning) {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      if (chrome.runtime.lastError) {
+        console.warn(
+          `updateConfigValue(): couldn't get active tab to set icon: ${
+            chrome.runtime.lastError.message
+          }.`
+        );
+        return;
+      }
+      chrome.pageAction.setIcon({
+        tabId: tabs[0].id,
+        path: isRunning
+          ? "images/disqus_eye_16.png"
+          : "images/disqus_eye_16_paused.png",
+      });
+    });
+  } // end of setIcon().
+}); // end of onInstalled listener.
