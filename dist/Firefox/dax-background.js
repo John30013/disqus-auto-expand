338fa47a473216806ee799216b8bd03d7bb0902f
John30013 <jmpisello@gmail.com>
2019-06-23 22:19:19 -0400

    v1.0.3 buld for Firefox.

--- dist/Firefox/dax-background.js
+++ dist/Firefox/dax-background.js
@@ -1,14 +1,13 @@
-let _config = defaultConfig;
-
-// Make sure the extension's options are stored when the extension starts up.
 browser.runtime.onInstalled.addListener(async () => {
+  let _config = defaultConfig;
+  // Make sure the extension's options are stored when the extension starts up.
+  // Pass `null` so we get everything in storage. This allows us to clean up
+  // obsolete config values (see else block below).
   try {
     _config = await browser.storage.sync.get(defaultConfig);
     if (!Object.keys(_config).length) {
       await browser.storage.sync.set(defaultConfig);
-      // console.debug("Stored default config: %o", defaultConfig);
     } else {
-      _config.doDebug && console.debug("Found existing config: %o", _config);
       // Check for and remove obsolete config items.
       let obsoleteKeys = [];
       for (let key in _config) {
@@ -18,11 +17,49 @@ browser.runtime.onInstalled.addListener(async () => {
       }
       if (obsoleteKeys.length) {
         await browser.storage.sync.remove(obsoleteKeys);
+        // removeIf(!allowDebug)
         _config.doDebug &&
           console.debug("--> removed obsolete config value(s)", obsoleteKeys);
+        // endRemoveIf(!allowDebug)
       }
     }
+    setIcon(!!_config.checkInterval);
   } catch (error) {
-    console.error(`Storage operation failed: ${error}.`);
+    console.info(`Storage operation failed: ${error}.`);
   }
-});
+
+  // Listen for messages from content and config scripts.
+  browser.runtime.onMessage.addListener(msg => {
+    // removeIf(!allowDebug)
+    _config.doDebug && console.debug("Got message: %o", msg);
+    // endRemoveIf(!allowDebug)
+    if (msg.action === "setIcon" && typeof msg.data !== "undefined") {
+      setIcon(!!msg.data);
+    }
+  });
+
+  // Set the extension's icon.
+  async function setIcon(isRunning) {
+    try {
+      const tabs = await browser.tabs.query({
+        active: true,
+        currentWindow: true,
+      });
+      // removeIf(!allowDebug)
+      _config.doDebug &&
+        console.debug(`--> Setting icon.`);
+      // endRemoveIf(!allowDebug)
+      browser.pageAction.setIcon({
+        tabId: tabs[0].id,
+        path: isRunning
+          ? "images/disqus_eye_16.png"
+          : "images/disqus_eye_16_paused.png",
+      });
+    } catch (error) {
+      console.info(
+        `updateConfigValue(): couldn't get active tab to set icon: ${error}.`
+      );
+      return;
+    }
+  } // end of setIcon().
+}); // end of onInstalled listener.
--- dist/Firefox/dax-config.js
+++ dist/Firefox/dax-config.js
@@ -1,45 +1,40 @@
-let _options = { ...defaultConfig };
+let _config = { ...defaultConfig };
 
-// removeIf(!allowDebug)
-function logDebug(message, ...params) {
-  browser.tabs
-    .query({ active: true, currentWindow: true })
-    .then(function(tabs) {
-      const messageData = {
-        action: "logDebug",
-        caller: "config",
-        message: message,
-        params: params,
-      };
-      browser.tabs.sendMessage(tabs[0].id, messageData);
-    })
-    .catch(error => {
-      console.error(
-        `logDebug(): couldn't get active tab to send a message to: ${error}.`
-      );
-    });
+// Initialize some text in the UI.
+initUiText();
+
+// Initialize configuration controls with values from storage
+// and listen for updates.
+getCurrentConfig();
+listenForUpdates();
+
+// Initialize the "Load all content" button and confirmation dialog.
+initLoadAllContent();
+
+/* ========== End of main code. ==========
+  ===== Helper functions follow. ===== */
+
+function initUiText() {
+  const manifest = browser.runtime.getManifest();
+  document.querySelectorAll(".version").forEach(elt => {
+    elt.innerHTML = manifest.version;
+  });
+  document.querySelectorAll("a.extensionStore").forEach(link => {
+    link.href =
+      "https://addons.mozilla.org/firefox/addon/disqus-auto-expander/";
+    link.innerText = "Firefox Add-ons site";
+  });
 }
-// endRemoveIf(!allowDebug)
 
-// Initialize some text in the config UI.
-const manifest = browser.runtime.getManifest();
-document.querySelectorAll(".version").forEach(elt => {
-  elt.innerHTML = manifest.version;
-});
-document.querySelectorAll("a.extensionStore").forEach(link => {
-  link.href = "https://addons.mozilla.org/firefox/addon/disqus-auto-expander/";
-  link.innerText = "Firefox Add-ons site";
-});
-
-// Initialize configuration controls from storage.
-browser.storage.sync
-  .get(defaultConfig)
-  .then(options => {
+async function getCurrentConfig() {
+  let config;
+  try {
+    config = await browser.storage.sync.get(defaultConfig);
     // removeIf(!allowDebug)
     logDebug("config.js loaded.");
-    logDebug("Setting options from storage: %o", options);
+    logDebug("Setting config from storage: %o", config);
     // endRemoveIf(!allowDebug)
-    for (let key in options) {
+    for (let key in config) {
       let input = document.getElementById(key);
       if (!input) {
         // removeIf(!allowDebug)
@@ -48,7 +43,7 @@ browser.storage.sync
         continue;
       }
       if (input.type === "checkbox") {
-        input.checked = options[key];
+        input.checked = config[key];
         if (key === "useDarkTheme") {
           document.body.classList.toggle("theme-dark", input.checked);
           // removeIf(!allowDebug)
@@ -62,70 +57,77 @@ browser.storage.sync
           key
         );
         // endRemoveIf(!allowDebug)
-      } else if (input.getAttribute("inputmode") === "numeric") {
-        input.value = "" + options[key];
+      } else if (key === "checkInterval") {
+        input.value = "" + config[key];
+        setIcon(!!config[key]);
         // removeIf(!allowDebug)
         logDebug("--> %s set to %s.", key, input.value);
         // endRemoveIf(!allowDebug)
+      } else if (key === "loadAllContent") {
+        // true =  operation is in progress, so the button should be diabled.
+        // false = operation is not in progress, so the button should be enanbled.
+        input.toggleAttribute("disabled", !!value);
+        input.disabled = !!value;
       }
     }
-  })
-  .catch(error => {
-    console.error("Couldn't initialize options from storage: %s", error);
-  });
+  } catch (error) {
+    console.info("Couldn't initialize config from storage: %s", error);
+  }
+} // end of getCurrentConfig().
 
 // Handle changes in the configuration controls.
-document.body.addEventListener("input", event => {
-  // removeIf(!allowDebug)
-  logDebug("Handling change event on %s", event.target.id);
-  // endRemoveIf(!allowDebug)
-  let target = event.target,
-    value = null,
-    typingDebounceTimer = null;
-  if (target.id === "checkInterval") {
-    if (typingDebounceTimer) {
-      clearTimeout(typingDebounceTimer);
+function listenForUpdates() {
+  document.body.addEventListener("input", event => {
+    // removeIf(!allowDebug)
+    logDebug("Handling change event on %s", event.target.id);
+    // endRemoveIf(!allowDebug)
+    let target = event.target,
+      value = null,
+      typingDebounceTimer = null,
+      debounceDelay = 3000;
+    if (target.id === "checkInterval") {
+      if (typingDebounceTimer) {
+        clearTimeout(typingDebounceTimer);
+      }
+      typingDebounceTimer = setTimeout(
+        target => {
+          if (target.validity.valid) {
+            updateConfigValue(target.id, +target.value);
+          } else {
+            // Restore the previous value after debounceDelay (msecs).
+            browser.storage.sync
+              .get(target.id)
+              .then(value => {
+                target.value = value[target.id];
+              })
+              .catch(error => {
+                console.info(
+                  "Couldn't get config value %s from storage: %s",
+                  target.id,
+                  error
+                );
+              });
+            return;
+          }
+        },
+        debounceDelay,
+        target
+      );
+    } else {
+      value = target.checked;
+      if (target.id === "useDarkTheme") {
+        document.body.classList.toggle("theme-dark", target.checked);
+        // removeIf(!allowDebug)
+        logDebug("--> document.body className: %s", document.body.className);
+        // endRemoveIf(!allowDebug)
+      }
+      updateConfigValue(target.id, value);
     }
-    typingDebounceTimer = setTimeout(
-      target => {
-        if (target.validity.valid) {
-          updateConfigValue(target.id, +target.value);
-        } else {
-          // Restore the previous value after 1 second.
-          browser.storage.sync
-            .get(target.id)
-            .then(value => {
-              target.value = value[target.id];
-            })
-            .catch(error => {
-              console.error(
-                "Couldn't get config value %s from storage: %s",
-                target.id,
-                error
-              );
-            });
-          return;
-        }
-      },
-      1000,
-      target
-    );
-  } else {
-    value = target.checked;
-    if (target.id === "useDarkTheme") {
-      document.body.classList.toggle("theme-dark", target.checked);
-      // removeIf(!allowDebug)
-      logDebug("--> document.body className: %s", document.body.className);
-      // endRemoveIf(!allowDebug)
-    }
-    updateConfigValue(target.id, value);
-  }
-});
+  });
 
-function updateConfigValue(key, value) {
-  browser.storage.sync
-    .set({ [key]: value })
-    .then(() => {
+  async function updateConfigValue(key, value) {
+    try {
+      await browser.storage.sync.set({ [key]: value });
       // removeIf(!allowDebug)
       logDebug(
         '--> Updated config option "%s" to (%s) "%s"',
@@ -134,22 +136,162 @@ function updateConfigValue(key, value) {
         value
       );
       // endRemoveIf(!allowDebug)
-    })
-    .catch(error => {
-      console.error(
+    } catch (error) {
+      console.info(
         `Couldn't store option ${key} with value ${value}: ${error}.`
       );
-    });
-  if (key !== "useDarkTheme") {
-    browser.tabs
-      .query({ active: true, currentWindow: true })
-      .then(tabs =>
-        browser.tabs.sendMessage(tabs[0].id, { action: "refreshOptions" })
-      )
-      .catch(error => {
-        console.error(
+    }
+    if (key !== "useDarkTheme") {
+      try {
+        const tabs = await browser.tabs.query({
+          active: true,
+          currentWindow: true,
+        });
+        browser.tabs.sendMessage(tabs[0].id, { action: "refreshConfig" });
+      } catch (error) {
+        console.info(
           `updateConfigValue(): couldn't get active tab to send a message to: ${error}.`
         );
-      });
+      }
+      if (key === "checkInterval") {
+        setIcon(!!value);
+      }
+    } // end of dark theme handling.
+  } // end of updateConfigValue().
+} // end of listenForUpdates().
+
+function setIcon(isRunning) {
+  // removeIf(!allowDebug)
+  logDebug(`[proxy] setIcon(${isRunning}): entering.`);
+  // endRemoveIf(!allowDebug)
+  browser.runtime.sendMessage({
+    action: "setIcon",
+    caller: "config",
+    data: isRunning,
+  });
+} // end of setIcon().
+
+// Initailize the "Load all content" button and confirmation dialog.
+function initLoadAllContent() {
+  // Initialize the confirmation dialog.
+  const dialog = document.getElementById("confirmLoadAllContent"),
+    dialogClickablesSelector = "a:link, button",
+    openClass = "dialog-open",
+    focusableClass = "dialog-focusable";
+
+  dialog.querySelectorAll(dialogClickablesSelector).forEach(elt => {
+    elt.classList.add(focusableClass);
+  });
+  closeDialog(dialog);
+
+  // Initialize the "Load all content" button. The button is disabled
+  // by default, and only an affirmative ping from the content script
+  // will enable it.
+  const button = document.getElementById("loadAllContent");
+  button.addEventListener("click", event => {
+    openDialog(dialog, event.target);
+  });
+  sendContentCommand({ action: "ping", caller: "config" }, reply => {
+    if (reply && reply.value === "pong") {
+      logDebug(
+        "Got reply from 'ping' request: %o; enabling Load all content button.",
+        reply
+      );
+      button.disabled = false;
+    }
+  });
+
+  /* ========== Helpers ========== */
+  function closeDialog(dialog) {
+    // removeIf(!allowDebug)
+    logDebug("closeDialog: %o", dialog);
+    // endRmoveIf(!allowDebug)
+    getNonDialogFocusables().forEach(elt => {
+      if (elt.dataset.tabindex) {
+        elt.tabIndex = elt.dataset.tabindex;
+      } else {
+        elt.tabIndex = 0;
+      }
+    });
+    dialog.classList.remove(openClass);
+    dialog.setAttribute("aria-hidden", "true");
+    dialog.querySelectorAll(dialogClickablesSelector).forEach(elt => {
+      elt.tabIndex = -1;
+    });
+  } // end of closeDialog().
+
+  function openDialog(dialog, opener) {
+    // removeIf(!allowDebug)
+    logDebug("openDialog: %o", dialog);
+    // endRemoveIf(!allowDebug)
+    // Disable focus on focusable elements outside the dialog.
+    getNonDialogFocusables().forEach(elt => {
+      if (elt.tabIndex) {
+        elt.dataset.tabindex = elt.tabIndex;
+      }
+      elt.tabIndex = -1;
+    });
+    // Show the dialog.
+    dialog.querySelectorAll(dialogClickablesSelector).forEach(elt => {
+      elt.tabIndex = 0;
+    });
+    dialog.tabIndex = -1;
+    dialog.focus();
+    dialog.classList.add(openClass);
+    dialog.setAttribute("aria-hidden", "false");
+    dialog.addEventListener("click", event => {
+      const source = event.target;
+      if (source.tagName === "BUTTON") {
+        event.preventDefault();
+        if (source.value === "yes") {
+          sendContentCommand({
+            action: "loadAllContent",
+            caller: "config",
+          });
+          window.close();
+        } else {
+          closeDialog(dialog);
+          opener.focus();
+        }
+      }
+    });
+  } // end of openDialog().
+
+  function getNonDialogFocusables() {
+    return Array.from(
+      document.querySelectorAll("a:link, button, input")
+    ).filter(elt => !elt.classList.contains(focusableClass));
+  } // end of getNonDialogClickables().
+} // end of initLoadAllContent().
+
+async function sendContentCommand(commandData, responseCallback) {
+  let operation = 'browser.tabs.query';
+  try {
+    const tabs = await browser.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+    operation = 'browser.tabs.sendMessage';
+    const response = await browser.tabs.sendMessage(
+      tabs[0].id,
+      commandData
+    );
+    if (responseCallback) {
+      responseCallback(response);
+    }
+  } catch (error) {
+    console.info(`sendContentCommand(): ${operation} failed: ${error}.`);
   }
+} // end of sendContentCommand().
+
+// removeIf(!allowDebug)
+function logDebug(message, ...params) {
+  console.debug(message, ...params);
+  sendContentCommand({
+    action: "logDebug",
+    caller: "config",
+    message: message,
+    params: params,
+  });
 }
+// endRemoveIf(!allowDebug)
