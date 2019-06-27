af198debc99c2a76beb78ae11015bdc082030370
John30013 <jmpisello@gmail.com>
2019-06-26 20:22:22 -0400

    Finish "Load entire discussion" button.

--- src/Chrome/dax-content.js
+++ src/Chrome/dax-content.js
@@ -1,9 +1,11 @@
+// "Global" variables and flags.
 let _config = { ...defaultConfig },
   _timer = null,
   _observer = null,
   _observedLinks = {},
   _linkCounter = 0,
-  _loadAllInitialized = false;
+  _loadAllInitialized = false,
+  _warnedAboutLoadAll = false;
 
 // Start the extension.
 refreshConfig(true);
@@ -20,8 +22,11 @@ processNewLinks();
 /* ===== End of main code. ===== */
 
 /* ===== Helper functions. ===== */
+/**
+ * Listens for messages from other scripts in the extension.
+ */
 function listenForMessages() {
-  chrome.runtime.onMessage.addListener((msg, sender, sendReply) => {
+  chrome.runtime.onMessage.addListener(msg => {
     if (msg.action === "refreshConfig") {
       refreshConfig(false);
     } else if (msg.action === "logDebug" && msg.message) {
@@ -31,73 +36,93 @@ function listenForMessages() {
       // endRemoveIf(!allowDebug)
     }
   });
-}
+} // end of listenForMessages().
 
+/**
+ * Creates the IntersectionObserver that handles collapes replies and new
+ * comments links that enter the viewport.
+ */
 function createObserver() {
   _observer = new IntersectionObserver(processObservedEntries, {
     threshold: 1.0,
   });
 
+  /**
+   * Activates (clicks) observed links when they enter the viewport, and
+   * removes them from observation.
+   * @param {IntersectionObserverEntry[]} entries - array of observed links that have either been
+   * initially put under observation or have entered the viewport.
+   */
   function processObservedEntries(entries) {
-    if (_config.checkInterval) {
-      let foundIntersects = false;
-      entries.forEach(entry => {
-        const link = entry.target,
-          luid = link.dataset.luid;
+    if (!_config.checkInterval) {
+      return;
+    }
+    let foundIntersects = false;
+    entries.forEach(entry => {
+      const link = entry.target,
+        luid = link.dataset.luid;
+      // removeIf(!allowDebug)
+      _config.doDebug &&
+        console.debug(
+          'Checking intersection of "%s" link %s',
+          link.className,
+          luid
+        );
+      // endRemoveIf(!allowDebug)
+      if (entry.isIntersecting) {
+        foundIntersects = true;
         // removeIf(!allowDebug)
         _config.doDebug &&
           console.debug(
-            'Checking intersection of "%s" link %s',
-            link.className,
-            luid
+            '--> link.classList: %o; link.innerText: "%s"',
+            link.classList,
+            link.innerText
           );
         // endRemoveIf(!allowDebug)
-        if (entry.isIntersecting) {
-          foundIntersects = true;
-          // removeIf(!allowDebug)
-          _config.doDebug &&
-            console.debug(
-              '--> link.classList: %o; link.innerText: "%s"',
-              link.classList,
-              link.innerText
-            );
-          // endRemoveIf(!allowDebug)
-          activateLink(link);
-          unobserveLink(link);
-          // removeIf(!allowDebug)
-          _config.doDebug &&
-            console.debug(
-              "--> Clicked %s (now %d observed)",
-              luid,
-              Object.keys(_observedLinks).length,
-              _observedLinks
-            );
-          // endRemoveIf(!allowDebug)
-        }
-      });
-      // Clean up old observed links. Disqus seems to output some (mostly "see more")
-      // links that become hidden before they are clicked (i.e., "zombie" links).
-      // This block unobserves all observed links that haven't been clicked after
-      // five minutes. If some non-zombie links get unobserved, they will be
-      // observed again the next time the processNewLinks timer fires.
-      if (foundIntersects) {
+        activateLink(link);
+        unobserveLink(link);
         // removeIf(!allowDebug)
-        _config.doDebug && console.debug("--> Checking for old links");
+        _config.doDebug &&
+          console.debug(
+            "--> Clicked %s (now %d observed)",
+            luid,
+            Object.keys(_observedLinks).length,
+            _observedLinks
+          );
         // endRemoveIf(!allowDebug)
-        const now = Date.now(),
-          maxAge = 5 * 60 * 1000;
-        Object.keys(_observedLinks)
-          .filter(luid => now - luid.substr(0, luid.indexOf("-")) >= maxAge)
-          .forEach(oldLuid => unobserveLink(_observedLinks[oldLuid], true));
       }
-    }
+    });
+    if (foundIntersects) {
+      /* Clean up old observed links. Disqus seems to output some (mostly "
+      see more") links that become hidden before they are clicked (a/k/a, 
+      "zombie" links). This block unobserves all observed links that haven't 
+      been clicked after five minutes. If some non-zombie links get unobserved,
+      they will be observed again the next time the processNewLinks() timer 
+      fires. */
+      // removeIf(!allowDebug)
+      _config.doDebug && console.debug("--> Checking for old links");
+      // endRemoveIf(!allowDebug)
+      const now = Date.now(),
+        maxAge = 5 * 60 * 1000;
+      Object.keys(_observedLinks)
+        .filter(luid => now - luid.substr(0, luid.indexOf("-")) >= maxAge)
+        .forEach(oldLuid => unobserveLink(_observedLinks[oldLuid], true));
+    } // end of link cleanup block.
   } // end of processObservedEntries().
 } // end of createObserver().
 
+/**
+ * Finds and registers new links to observe for intersection with the viewport.
+ * This function also executes the main logic loop, depending on the value of
+ * the checkInterval configuration option.
+ */
 function processNewLinks() {
-  // Since processNewLinks() can be called by refreshConfig() when checkInterval is
-  // set to zero, clear any pending timeout first. It's a no-op if this call is
-  // the result of the timer timing out.
+  /* Since processNewLinks() is called by refreshConfig() when checkInterval is
+  zero, clear any pending timeout first. It's a no-op if this call is due to 
+  the timer timing out.
+  Note: if _loadAllInitialized is `false`, we keep the loop going until the
+  content loads, so we can install the "Load all content" button at the top of
+  the posts. (Also see the setTimout() call at the end of this method.) */
   _timer && clearTimeout(_timer);
   if (!_config.checkInterval && _loadAllInitialized) {
     // removeIf(!allowDebug)
@@ -120,7 +145,7 @@ function processNewLinks() {
     }
   }
 
-  // Make external links open in a new browser tab/window.
+  // Force external URLs open in a new browser tab/window.
   if (_config.openInNewWindow) {
     const extLinkSelector =
       "a[href*='disq.us/url?'][rel*='noopener']:not([target])";
@@ -134,11 +159,20 @@ function processNewLinks() {
     });
   }
 
-  // Reprocess after the checkInterval.
-  _timer = setTimeout(processNewLinks, _config.checkInterval * 1000);
+  // Reprocess after the checkInterval (or after 5 seconds if the "Load
+  // all content" button hasn't yet been initialized).
+  _timer = setTimeout(
+    processNewLinks,
+    (!_loadAllInitialized ? 5 : _config.checkInterval) * 1000
+  );
 
-  // Observe a link. Instructs the IntersectionOserver to start observing the
-  // link and does some record keeping.
+  /* ===== Helper functions ===== */
+  /**
+   * Instructs the IntersectionOserver to start observing the link, and does
+   * some record keeping.
+   * @param {HTMLAnchorElement} link - the link to observe for intersection with
+   * the viewport.
+   */
   function observeLink(link) {
     let luid = link.dataset.luid;
     if (luid) {
@@ -158,9 +192,16 @@ function processNewLinks() {
         _observedLinks
       );
     // endRemoveIf(!allowDebug)
-  }
-}
+  } // end of observeLink().
+} // end of processNewLinks().
 
+/**
+ * Gets the latest configuration values from storage, and optionally asks the
+ * background script to update the extension's icon based on the value of
+ * checkInterval.
+ * @param {Boolean} setIcon - whether to change the extension's icon based on
+ * the value of the checkInterval config value (0 -> paused; > 0 -> active).
+ */
 function refreshConfig(setIcon) {
   chrome.storage.sync.get(defaultConfig, config => {
     if (chrome.runtime.lastError) {
@@ -191,7 +232,11 @@ function refreshConfig(setIcon) {
 }
 
 /**
- * Remove a link from observation and record-keeping. (Reverses proecessNewLinks()#observeLink().)
+ * Remove a link from observation and record-keeping.
+ * (Reverses proecessNewLinks()#observeLink().)
+ * @param {HTMLAnchorElement} link - a link currently under observation.
+ * @param {Boolean} removeDaxTags - whether to remove "bookkeeping" classes set
+ * on the link by tagLink() and activateLink().
  */
 function unobserveLink(link, removeDaxTags) {
   const luid = link.dataset.luid;
@@ -219,6 +264,11 @@ function unobserveLink(link, removeDaxTags) {
   // endRemoveIf(!allowDebug)
 } // end of unobserveLink().
 
+/**
+ * Finds new links to observe, based on the configuration options. This
+ * function is called by both processNewLinks() and loadAllContent().
+ * @param {Object} config
+ */
 function findNewLinks(config) {
   const newLinks = [];
   // Find new "See more replies" links.
@@ -260,8 +310,13 @@ function findNewLinks(config) {
       .forEach(elt => newLinks.push(elt));
   }
   return newLinks;
-}
+} // end of findNewLinks().
 
+/**
+ * Marks the link as being under observation for intersection with the
+ * viewport.
+ * @param {HTMLAnchorElement} link - the link under observation.
+ */
 function tagLink(link) {
   luid = `${Date.now()}-${_linkCounter++}`;
   link.setAttribute("data-luid", luid);
@@ -270,15 +325,23 @@ function tagLink(link) {
     link.title = `[tagged] ${link.title}`;
   }
   return luid;
-}
+} // end of tagLink().
 
+/**
+ * Marks the link as having been activated (clicked).
+ * @param {HTMLAnchorElement} link - the link that has been clicked.
+ */
 function activateLink(link) {
   link.click();
   link.classList.add("dax-clicked");
   link.title = link.title.replace("[tagged] ", "[clicked] ");
-}
+} // end of activateLink().
 
+/**
+ * Installs and enables the "Load all content" button.
+ */
 function initLoadAllContent() {
+  console.debug("initLoadAllContent(): entering");
   const button = document.createElement("button");
   button.innerText = "Load entire discussion";
   button.id = "dax-loadAll";
@@ -287,8 +350,15 @@ function initLoadAllContent() {
   });
   document.getElementById("posts").prepend(button);
   _loadAllInitialized = true;
-}
+  console.debug("initLoadAllContent(): exiting.");
+} // end of initLoadAllContent().
 
+/**
+ * Recursively loads all of the available content in the discussion.
+ * @param {Number} iteration - the count of how many times this method has been
+ * called. Used to vary the status message shown to the user while the content
+ * is being loaded.
+ */
 function loadAllContent(iteration) {
   // removeIf(!allowDebug)
   _config.doDebug && console.debug("loadAllContent(): entering.");
@@ -298,22 +368,37 @@ function loadAllContent(iteration) {
 
   iteration = iteration || 0;
   if (iteration === 0) {
-    const commentCount = parseInt(
-      document.querySelector(".comment-count").innerText
-    );
-    if (
-      commentCount > 500 &&
-      !confirm(
-        `Loading this entire discussion will take some time and
-  could consume a lot of memory and data. Your browser 
-  might become very slow, or stop responding at all.
-
-Do you want to proceed?`
-      )
-    ) {
-      return;
+    /* Try to get the number of comments. When Disqus is embedded on a page, the
+    comment count is in a div.comment-count element within the iframe; when on
+    the Disqus site itself, we have to get it from the disqus-threadData JSON
+    object, which is more expensive because the object is potentially large. */
+    let commentCount = -1;
+    let commentCountElt = document.querySelector(".comment-count");
+    if (commentCountElt) {
+      commentCount = parseInt(commentCountElt.innerText, 10);
+    } else {
+      commentCountElt = document.getElementById("disqus-threadData");
+      try {
+        commentCount = JSON.parse(commentCountElt.innerText).cursor.total;
+      } catch (error) {
+        console.info("Couldn't get comment count from JSON: %s", error);
+      }
     }
-  }
+    if (commentCount > 500 && !_warnedAboutLoadAll) {
+      _warnedAboutLoadAll = true;
+      if (
+        !confirm(
+          `Loading this entire discussion will take some time and
+          could consume a lot of memory and data. Your browser 
+          might become very slow, or stop responding at all.
+
+        Do you want to proceed?`
+        )
+      ) {
+        return;
+      }
+    }
+  } // end of block to handle first iteration.
   const button = document.getElementById("dax-loadAll");
   const newLinks = findNewLinks();
   // removeIf(!allowDebug)
@@ -341,8 +426,17 @@ Do you want to proceed?`
     const delay = Math.floor(1000 * (5 + (_config.checkInterval * 5) / 30));
     _timer = setTimeout(loadAllContent, delay, iteration + 1);
   } else {
-    button.classList.remove("processing");
-    button.innerText = "Load entire discussion";
-    processNewLinks();
+    button.classList.add("complete");
+    button.innerText = "The entire discussion is now loaded.";
+    setTimeout(
+      button => {
+        button.classList.remove("processing", "complete");
+        button.innerText = "Load entire discussion";
+        button.blur();
+        processNewLinks();
+      },
+      4000, // see
+      button
+    );
   }
 }
